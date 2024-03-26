8548c78f970f06ebdba36f47835eeb7f342dab07
Richard Washington <richard.washington@taxassist.co.uk>
2024-03-26 15:12:30 +0000

    :sparkles: Add and test MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), solidJs()]
+  integrations: [react(), solidJs(), mdx()]
 });
