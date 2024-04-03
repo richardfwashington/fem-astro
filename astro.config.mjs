93b910a7dd3b1cdbc1439e1d2cb60b5eaf805ade
Richard Washington <richard.washington@taxassist.co.uk>
2024-04-03 16:55:41 +0100

    :rocket: Add adapter for node to test production locally

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import solid from '@astrojs/solid-js';
-
 import mdx from '@astrojs/mdx';
+import node from "@astrojs/node";
 
+// https://astro.build/config
 export default defineConfig({
-	output: 'hybrid',
-	integrations: [
-		solid({
-			include: ['**/solid/*'],
-		}),
-		react({
-			include: ['**/react/*'],
-		}),
-		mdx(),
-	],
-	site: 'https://astro-frontend-masters.netlify.app',
+  output: 'hybrid',
+  integrations: [solid({
+    include: ['**/solid/*']
+  }), react({
+    include: ['**/react/*']
+  }), mdx()],
+  site: 'https://astro-frontend-masters.netlify.app',
+  adapter: node({
+    mode: "standalone"
+  })
 });
