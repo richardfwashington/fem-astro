f013c095ba42840b463fa1f26c0022d1a9f4bf05
Richard Washington <richard.washington@taxassist.co.uk>
2024-04-08 16:53:59 +0100

    :wrench: Enable vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,28 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import solid from '@astrojs/solid-js';
 import mdx from '@astrojs/mdx';
-import node from "@astrojs/node";
+import node from '@astrojs/node';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  integrations: [solid({
-    include: ['**/solid/*']
-  }), react({
-    include: ['**/react/*']
-  }), mdx()],
-  site: 'https://fem-astro-tau.vercel.app',
-  adapter: vercel(),
-  prefetch: true,
+	output: 'hybrid',
+	integrations: [
+		solid({
+			include: ['**/solid/*'],
+		}),
+		react({
+			include: ['**/react/*'],
+		}),
+		mdx(),
+	],
+	site: 'https://fem-astro-tau.vercel.app',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+		maxDuration: 8,
+	}),
+	prefetch: true,
 });
